b28d35c241185c4e9703bf24b7866b4d39ba4fb2
Carlos Huchim Ahumada <huchim@live.com.mx>
2016-10-04 17:59:10 -0500

    Creación de la rutina para compilar la biblioteca.

--- src/Jaguar.Core.DAL/Properties/AssemblyInfo.cs
+++ src/Jaguar.Core.DAL/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Jaguar.Core.DAL")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Biblioteca que permite utilizar cualquier ORM en la aplicación final, funcionando como una capa extra.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Jaguar")]
 [assembly: AssemblyProduct("Jaguar.Core.DAL")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCopyright("Copyright © Jaguar 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
