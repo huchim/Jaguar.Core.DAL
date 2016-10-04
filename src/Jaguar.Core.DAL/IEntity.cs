5c109e1b7849644d51c0faa84d8edbc7436fe32e
Carlos Huchim Ahumada <huchim@live.com.mx>
2016-10-04 11:22:31 -0500

    Creación del contrato con la entidad.

---
+++ src/Jaguar.Core.DAL/IEntity.cs
@@ -0,0 +1,25 @@
+﻿namespace Jaguar.Core.DAL
+{
+    using System;
+
+    /// <summary>
+    /// Representa las propiedades que debe tener como mínimo la entidad a manipular.
+    /// </summary>
+    public interface IEntity
+    {
+        /// <summary>
+        /// Obtiene o establece el identificador del registro.
+        /// </summary>
+        Guid Id { get; set; }
+
+        /// <summary>
+        /// Obtiene o establece la fecha de creación del registro.
+        /// </summary>
+        DateTime Created { get; set; }
+
+        /// <summary>
+        /// Obtiene o establece la fecha de modificación del registro.
+        /// </summary>
+        DateTime Modified { get; set; }
+    }
+}
